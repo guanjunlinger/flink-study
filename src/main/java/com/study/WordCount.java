082b03cbfb6f856d7fe87f849e3671fc9fd1f032
guanjun <1126964622@qq.com>
2021-02-07 23:24:36 +0800

    DataSet

--- src/main/java/com/study/WordCount.java
+++ src/main/java/com/study/WordCount.java
@@ -1,24 +1,23 @@
 package com.study;
 
+import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.api.java.utils.ParameterTool;
-import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 
 
 public class WordCount {
 
-    public static void main(String[] args) {
-        final ParameterTool params = ParameterTool.fromArgs(args);
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.getConfig().setGlobalJobParameters(params);
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
-        DataStream<String> text = env.readTextFile(params.get("input"))
-                .flatMap((a,b)->{
+        env.readTextFile("")
+                .flatMap((String a, Collector<Tuple2<String, Integer>> b) -> {
                     String[] words = a.split(" ");
-                    for(String word : words){
-                         b.collect(new Tuple2(word,1));
+                    for (String word : words) {
+                        b.collect(new Tuple2<>(word, 1));
 
-                    } }).keyBy(value->value.f0));
+                    }
+                }).groupBy(value -> value.f0).reduce((tuple1, tuple2) ->
+                new Tuple2<>(tuple1.f0, tuple1.f1 + tuple2.f1)).print();
     }
 }
