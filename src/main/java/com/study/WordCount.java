8ab070c2d61c69559186139d5e110d92d59d8479
guanjun <1126964622@qq.com>
2021-02-13 17:49:39 +0800

    DataSet

--- src/main/java/com/study/WordCount.java
+++ src/main/java/com/study/WordCount.java
@@ -2,10 +2,16 @@ package com.study;
 
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.functions.FlatMapIterator;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.util.Collector;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+/**
+ * flink run -c com.study.WordCount  D:\gitRepository\flink-study\target\fink-study-1.0-SNAPSHOT.jar
+ */
 public class WordCount {
 
     public static void main(String[] args) throws Exception {
@@ -14,12 +20,19 @@ public class WordCount {
         DataSet<String> text = env.fromElements(
                 "Who's there?",
                 "I think I hear them. Stand, ho! Who's there?");
-        text.flatMap((String a, Collector<Tuple2<String, Integer>> b) -> {
-            String[] words = a.split(" ");
-            for (String word : words) {
-                b.collect(new Tuple2<>(word, 1));
+        text.flatMap(new MyFlatMapIterator()).groupBy(0).sum(1).print();
+    }
 
+    public static class MyFlatMapIterator extends FlatMapIterator<String, Tuple2<String, Integer>> {
+
+        @Override
+        public Iterator<Tuple2<String, Integer>> flatMap(String s) {
+            String[] words = s.split(" ");
+            List<Tuple2<String, Integer>> list = new ArrayList<>();
+            for (String word : words) {
+                list.add(new Tuple2<>(word, 1));
             }
-        }).groupBy(0).sum(1).print();
+            return list.iterator();
+        }
     }
 }
