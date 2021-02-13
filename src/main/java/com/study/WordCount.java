42cd00ffd70fb2acdb853dce7115afb639cabfef
guanjun <1126964622@qq.com>
2021-02-13 16:23:15 +0800

    DataSet

--- src/main/java/com/study/WordCount.java
+++ src/main/java/com/study/WordCount.java
@@ -1,5 +1,6 @@
 package com.study;
 
+import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.util.Collector;
@@ -10,14 +11,15 @@ public class WordCount {
     public static void main(String[] args) throws Exception {
         ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
-        env.readTextFile("")
-                .flatMap((String a, Collector<Tuple2<String, Integer>> b) -> {
-                    String[] words = a.split(" ");
-                    for (String word : words) {
-                        b.collect(new Tuple2<>(word, 1));
+        DataSet<String> text = env.fromElements(
+                "Who's there?",
+                "I think I hear them. Stand, ho! Who's there?");
+        text.flatMap((String a, Collector<Tuple2<String, Integer>> b) -> {
+            String[] words = a.split(" ");
+            for (String word : words) {
+                b.collect(new Tuple2<>(word, 1));
 
-                    }
-                }).groupBy(value -> value.f0).reduce((tuple1, tuple2) ->
-                new Tuple2<>(tuple1.f0, tuple1.f1 + tuple2.f1)).print();
+            }
+        }).groupBy(0).sum(1).print();
     }
 }
